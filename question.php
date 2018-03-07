1d1176426093b454331dc1916631efdb4ddc6cdc
AndreVictor60 <andre.victor60@free.fr>
2018-03-07 15:57:17 +0100

    Victorien

--- question.php
+++ question.php
@@ -9,88 +9,88 @@ include_once 'controllers/questionController.php';
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="https://bootswatch.com/4/united/bootstrap.css" />
-    <link rel="stylesheet" href="assets/css/master.css" />
-    <title>Test-e-quizz</title>
-  <body>
-    <div class="container-fluid">
-      <div class="row">  
-        <h1 class="w-100 text-center display-1">Quizz-in</h1>
-      </div>
-      <div class="row">
-        <div class="offset-3 col-6">  
-          <form method="POST" action="resultats.php">
-            <div class = "card border-primary mb-3 visible" id="card-0">
-              <div class = "card-header">
-                <h2 class="h3">
-                  Vous êtes prêts ?
-                </h2>
-              </div>
-              <div class="card-body">
-                <p>Faîtes bien attention si vous cliquez sur une réponse elle sera validée !</p>
-                <div class="d-flex justify-content-end button">  
-                  <button type="button" name="next" class="btn btn-primary nextButton"  id="0">Valider</button>
-                </div>
-              </div>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="https://bootswatch.com/4/united/bootstrap.css" />
+        <link rel="stylesheet" href="assets/css/master.css" />
+        <title>Test-e-quizz</title>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <h1 class="w-100 text-center display-1">Quizz-in</h1>
             </div>
-            <?php foreach ($questionsList as $question) { ?>
-                <div class = "card border-primary mb-3 hidden question" id="card-<?= $question->id ?>">
-                  <div class = "card-header">
-                    <h2 class="h3">
-                      <?= $question->id ?> / 10 - 
-                      <?= $question->question; ?>           
-                    </h2>
-                  </div>
-                  <div class="card-body">
-                      <?php
-                      if ($question->picture) {
-                          ?>
-                        <div class="col-12 d-flex justify-content-center button"> 
-                          <img src="assets/img/imgQ8.jpg" class="img-fluid w-25 img-thumbnail" />
+            <div class="row">
+                <div class="offset-3 col-6">
+                    <form method="POST" action="resultats.php">
+                        <div class = "card border-primary mb-3 visible" id="card-0">
+                            <div class = "card-header">
+                                <h2 class="h3">
+                                    Vous êtes prêts ?
+                                </h2>
+                            </div>
+                            <div class="card-body">
+                                <p>Faîtes bien attention si vous cliquez sur une réponse elle sera validée !</p>
+                                <div class="d-flex justify-content-end button">
+                                    <button type="button" name="next" class="btn btn-primary nextButton"  id="0">Valider</button>
+                                </div>
+                            </div>
                         </div>
-                        <?php
-                    }
-                    foreach ($answersList as $answer) {
-                        if ($answer->idQuestion == $question->id) {
-                            ?>
-                            <p class="card-text">
-                              <input data-answer="<?= $answer->isCorrect; ?>" type="radio" name="question<?= $answer->idQuestion; ?>" value="<?= $answer->id; ?>">
-                              <label data-iscorrect="<?= $answer->isCorrect; ?>" class="pl-3" for="question<?= $answer->idQuestion; ?>"><?= $answer->answer; ?></label>
-                            </p>
+                        <?php foreach ($questionsList as $question) { ?>
+                            <div class = "card border-primary mb-3 hidden question" id="card-<?= $question->id ?>">
+                                <div class = "card-header">
+                                    <h2 class="h3">
+                                        <?= $question->id ?> / 10 -
+                                        <?= $question->question; ?>
+                                    </h2>
+                                </div>
+                                <div class="card-body">
+                                    <?php
+                                    if ($question->picture) {
+                                        ?>
+                                        <div class="col-12 d-flex justify-content-center button">
+                                            <img src="assets/img/imgQ8.jpg" class="img-fluid w-25 img-thumbnail" />
+                                        </div>
+                                        <?php
+                                    }
+                                    foreach ($answersList as $answer) {
+                                        if ($answer->idQuestion == $question->id) {
+                                            ?>
+                                            <p class="card-text">
+                                                <input data-answer="<?= $answer->isCorrect; ?>" type="radio" name="question<?= $answer->idQuestion; ?>" value="<?= $answer->id; ?>">
+                                                <label data-iscorrect="<?= $answer->isCorrect; ?>" class="pl-3" for="question<?= $answer->idQuestion; ?>"><?= $answer->answer; ?></label>
+                                            </p>
+                                            <?php
+                                        }
+                                    }
+                                    if ($question->id == 10) {
+                                        ?>
+                                        <div class="d-flex justify-content-end button">
+                                            <button type="submit" name="validate" class="btn btn-primary nextButton" id="<?= $question->id ?>">Valider</button>
+                                        </div>
+                                        <?php
+                                    } else {
+                                        ?>
+                                        <div class="d-flex justify-content-end button">
+                                            <button type="button" name="next" class="btn btn-primary nextButton" id="<?= $question->id ?>">Question suivante</button>
+                                        </div>
+                                        <?php
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                            <div class="alert  alert-info answer hidden" id="description-<?= $question->id; ?>">
+                                <h3 class="alert-heading">Le saviez-vous ?</h3>
+                                <p class="mb-0"><?= $question->description ?>.</p>
+                            </div>
                             <?php
                         }
-                    }
-                    if ($question->id == 10) {
                         ?>
-                        <div class="d-flex justify-content-end button">  
-                          <button type="submit" name="validate" class="btn btn-primary nextButton" id="<?= $question->id ?>">Valider</button>
-                        </div>
-                        <?php
-                    } else {
-                        ?>
-                        <div class="d-flex justify-content-end button">  
-                          <button type="button" name="next" class="btn btn-primary nextButton" id="<?= $question->id ?>">Question suivante</button>
-                        </div>
-                        <?php
-                    }
-                    ?>
-                  </div>
+                    </form>
                 </div>
-                <div class="alert  alert-info answer hidden" id="description-<?= $question->id; ?>">                    
-                  <h3 class="alert-heading">Le saviez-vous ?</h3>
-                  <p class="mb-0"><?= $question->description ?>.</p>                    
-                </div>
-                <?php
-            }
-            ?>
-          </form>
+            </div>
         </div>
-      </div>
-    </div>
-  </body>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <script src="assets/js/master.js" type="text/javascript"></script>
+    </body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="assets/js/master.js" type="text/javascript"></script>
 </html>
